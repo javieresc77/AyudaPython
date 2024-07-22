9b8f4e298e31cda817c8462641603bcd8b5d4539
Javier Soto <javieresc77@gmail.com>
2024-07-21 21:23:47 -0400

    Primeros pasos de Print

---
+++ Python_Tipos_de_Print.py
@@ -0,0 +1,43 @@
+
+pesos_por_hora = 200
+llegada = 20
+salida = 22
+precio = (salida-llegada) *pesos_por_hora
+print(precio)
+
+print("Hola Mundo")
+
+mensaje = "Hola Mundo 2.0"
+print(mensaje)
+
+
+#print ((3+5//4-2)-2**4+3(7-2))
+
+fahrenheit = 91
+celsius = (fahrenheit -32) * 5/9
+print(celsius)
+
+temperatura = 33
+print(temperatura >= 30)
+
+episodio = 3
+print("episodio")
+print(episodio)
+
+print("Episodio" , episodio , "del libro")
+
+nombre ="Cristian"
+edad =38
+print("Soy" , nombre, end=" ")
+print("y tengo", edad, "años")
+print("cumplidos")
+
+print("Este print", end =" :D ")
+print("no cambia", "de linea", end = " ;)")
+
+msj1 = "Hoy habrá"
+temp = 34
+msj2 = "grados"
+print(msj1 , temp,msj2)
+
+print("-El tesoro", "está", "en", sep="...")
